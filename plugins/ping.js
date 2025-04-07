d4de0438eea82eff16a1fb38bcb820aa97ce2074
Pkdriller <benardwachira49@gmail.com>
2025-04-08 02:38:37 +0300

    Add files via upload

---
+++ plugins/ping.js
@@ -0,0 +1,62 @@
+const config = require('../config');
+const { cmd, commands } = require('../command');
+
+cmd({
+    pattern: "ping",
+    alias: "speed",
+    desc: "Check bot's response time.",
+    category: "main",
+    react: "🌏",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, reply }) => {
+    try {
+        const startTime = Date.now();
+
+        // Add a short delay
+        await new Promise(resolve => setTimeout(resolve, 100)); // 100M/s delay
+
+        const endTime = Date.now();
+        const ping = endTime - startTime;
+
+        // Send the ping result
+        await conn.sendMessage(from, { 
+            text: `*VORTEX-XMD 😌 SPEED: ${ping}M/s*`, 
+            contextInfo: {
+                mentionedJid: [m.sender],
+                forwardingScore: 999,
+                isForwarded: true,
+                forwardedNewsletterMessageInfo: {
+                    newsletterJid: '120363352087070233@newsletter',
+                    newsletterName: 'VORTEX-XMDING SPEED 😌',
+                    serverMessageId: 143
+                }
+            }
+        }, { quoted: mek });
+    } catch (e) {
+        console.error(e);
+        reply(`An error occurred: ${e.message}`);
+    }
+});
+
+// ping2 
+
+cmd({
+    pattern: "ping2",
+    desc: "Check bot's response time.",
+    category: "main",
+    react: "🎃",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        const startTime = Date.now()
+        const message = await conn.sendMessage(from, { text: '> *SPEED...*' })
+        const endTime = Date.now()
+        const ping = endTime - startTime
+        await conn.sendMessage(from, { text: `*VORTEX-XMD 😌😌 SPEED : ${ping}M/s*` }, { quoted: message })
+    } catch (e) {
+        console.log(e)
+        reply(`${e}`)
+    }
+})
---
+++ plugins/support.js
@@ -0,0 +1,80 @@
+const config = require('../config');
+const { cmd, commands } = require('../command');
+const os = require("os");
+const { runtime } = require('../lib/functions');
+const axios = require('axios');
+const more = String.fromCharCode(8206)
+const readMore = more.repeat(4001)
+
+cmd({
+    pattern: "support",
+    alias : "version",
+    desc: " allmenu",
+    category: "allmenu",
+    react: "🫅",
+    filename: __filename
+}, 
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+
+let dec = `    
+⟣──────────────────⟢
+▧ *ᴄʀᴇᴀᴛᴏʀ* : *ᴍʀ Hanstz  (🇹🇿)*
+▧ *ᴍᴏᴅᴇ* : *${config.MODE}*
+▧ *ᴘʀᴇғɪx* : *${config.PREFIX}*
+▧ *ʀᴀᴍ* : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+▧ *ᴠᴇʀsɪᴏɴ* : *V.5* ⚡
+▧ *ᴜᴘᴛɪᴍᴇ* : ${runtime(process.uptime())}
+
+⟣──────────────────⟢
+
+> VORTEX-𝐗𝐌𝐃 
+
+⟣──────────────────⟢
+${readMore}
+\`CHANNEL🛠️\`
+https://tinyurl.com/26kh6jss
+
+\`GROUP\` 👥
+
+https://tinyurl.com/26kh6jss
+
+\`VORTEX 𝐗𝐌𝐃\` *Dev🧑‍💻*
+
+wa.me/+255760774888 ?text=Support!
+
+⟣──────────────────⟢
+
+`;
+
+await conn.sendMessage(
+            from,
+            {
+                image: { url: `https://i.ibb.co/BKYt9f00/caseyrhodes-tech.jpg` },
+                caption: dec,
+                contextInfo: {
+                    mentionedJid: [m.sender],
+                    forwardingScore: 999,
+                    isForwarded: true,
+                    forwardedNewsletterMessageInfo: {
+                        newsletterJid: '120363352087070233@newsletter',
+                        newsletterName: '『 VORTEX-𝐗𝐌𝐃 』',
+                        serverMessageId: 143
+                    }
+                }
+            },
+            { quoted: mek }
+        );
+
+        // Send audio
+        await conn.sendMessage(from, {
+            audio: { url: 'https://files.catbox.moe/a1sh4u.mp3' },
+            mimetype: 'audio/mp4',
+            ptt: true
+        }, { quoted: mek });
+        
+    } catch (e) {
+        console.log(e);
+        reply(`${e}`);
+    }
+});
