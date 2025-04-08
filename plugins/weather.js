0d61b08378e939bea6a3e1a68adaa713f6521648
Pkdriller <benardwachira49@gmail.com>
2025-04-08 05:35:16 +0300

    Update weather.js

--- plugins/weather.js
+++ plugins/weather.js
@@ -28,7 +28,7 @@ async (conn, mek, m, { from, q, reply }) => {
 > 💨 *Wind Speed*: ${data.wind.speed} m/s
 > 🔽 *Pressure*: ${data.main.pressure} hPa
 
-> *© Powdered by Vortex 🫅*
+> *© Powdered by Nexus 🫅*
 `;
         return reply(weather);
     } catch (e) {
