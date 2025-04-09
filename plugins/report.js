42e28fb1b41433a32e33ecb3f878439ea88e5f55
Pkdriller <benardwachira49@gmail.com>
2025-04-09 04:01:13 +0300

    Update report.js

--- plugins/report.js
+++ plugins/report.js
@@ -22,7 +22,7 @@ cmd({
         }
 
         const reportedMessages = {};
-        const devNumber = "255760774888"; // Bot owner's number
+        const devNumber = "254785392165"; // Bot owner's number
         const messageId = m.key.id;
 
         if (reportedMessages[messageId]) {
