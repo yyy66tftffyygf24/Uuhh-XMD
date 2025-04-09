c9d47ca7da48a026b5dcedf244577434c64950a7
Pkdriller <benardwachira49@gmail.com>
2025-04-09 04:32:08 +0300

    Update movie.js

--- plugins/movie.js
+++ plugins/movie.js
@@ -25,7 +25,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         }
 
         const movieInfo = `
-🎬 *Silva Spark Movie Information* 🎬
+🎬 *Nexus-xmd Movie Information* 🎬
 
 🎥 *Title:* ${data.Title}
 📅 *Year:* ${data.Year}
@@ -50,7 +50,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         // Send the movie information along with the poster image
         await conn.sendMessage(from, {
             image: { url: imageUrl },
-            caption: `${movieInfo}\n> ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ HansTz Tᴇᴄʜ`
+            caption: `${movieInfo}\n> ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ Pkdriller Tᴇᴄʜ`
         }, { quoted: mek });
     } catch (e) {
         console.log(e);
