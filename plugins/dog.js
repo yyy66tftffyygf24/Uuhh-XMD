eb6abbd1bc7b4d7785eb219822c84ad7b64d84e8
Pkdriller <benardwachira49@gmail.com>
2025-04-09 04:45:51 +0300

    Update dog.js

--- plugins/dog.js
+++ plugins/dog.js
@@ -14,7 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const response = await axios.get(apiUrl);
         const data = response.data;
 
-        await conn.sendMessage(from, { image: { url: data.message }, caption: '> *© Vᴏʀᴛᴇx xᴍᴅ  ' }, { quoted: mek });
+        await conn.sendMessage(from, { image: { url: data.message }, caption: '> *© Nexus xᴍᴅ  ' }, { quoted: mek });
     } catch (e) {
         console.log(e); // ❯❯ Vᴏʀᴛᴇx xᴍᴅ 
         reply(`єяяσя ƒєт¢нιηg ∂σg ιмαgє: ${e.message}`);
