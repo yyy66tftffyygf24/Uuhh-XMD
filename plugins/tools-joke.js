4d43796754485535f12208ea90e594876db724df
Pkdriller <benardwachira49@gmail.com>
2025-04-11 00:01:41 +0300

    Update tools-joke.js

--- plugins/tools-joke.js
+++ plugins/tools-joke.js
@@ -18,7 +18,7 @@ cmd({
       return reply("❌ Failed to fetch a joke. Please try again.");
     }
 
-    const jokeMessage = `🤣 *Here's a random joke for you!* 🤣\n\n*${joke.setup}*\n\n${joke.punchline} 😆\n\n> *© Pᴏᴡᴇʀᴇᴅ Bʏᴇ Sʜᴀʙᴀɴ Mᴅ*`;
+    const jokeMessage = `🤣 *Here's a random joke for you!* 🤣\n\n*${joke.setup}*\n\n${joke.punchline} 😆\n\n> *© Pᴏᴡᴇʀᴇᴅ By NEXUS-XMD*`;
 
     return reply(jokeMessage);
   } catch (error) {
