168db36f612fccbef40a37d500e4f7aea68ee531
Pkdriller <benardwachira49@gmail.com>
2025-04-10 03:40:52 +0300

    Update emix-fun.js

--- plugins/emix-fun.js
+++ plugins/emix-fun.js
@@ -31,7 +31,7 @@ cmd({
         let buffer = await getBuffer(imageUrl);
         let sticker = new Sticker(buffer, {
             pack: "Emoji Mix",
-            author: "SHABAN-MD",
+            author: "NEXUS-XMD",
             type: StickerTypes.FULL,
             categories: ["🤩", "🎉"],
             quality: 75,
