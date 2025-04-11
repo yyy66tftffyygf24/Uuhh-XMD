69592b74fcb2667b6872ca1b877180d8895ce1f3
Pkdriller <benardwachira49@gmail.com>
2025-04-11 03:28:18 +0300

    Update logo-menu.js

--- plugins/logo-menu.js
+++ plugins/logo-menu.js
@@ -49,15 +49,15 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://cdn.ironman.my.id/i/m8r9ux.jpg` },
+                image: { url: `https://i.ibb.co/TDF3kZf1/nexus-xmd.jpg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363318387454868@newsletter',
-                        newsletterName: "𝐀ɭīī 𝐌Ɗ 𝐒ʊ̊𝐏𝐏๏፝֟ɼʈ⎯꯭̽🪼🧸₊",
+                        newsletterJid: '120363288304618280@newsletter',
+                        newsletterName: "NEXUS x𝐌Ɗ 𝐒ʊ̊𝐏𝐏๏፝֟ɼʈ⎯꯭̽🪼🧸₊",
                         serverMessageId: 143
                     }
                 }
