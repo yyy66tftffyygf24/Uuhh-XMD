ab6293b331ed1cf549dc57da0a205af6c9d5fff2
Pkdriller <benardwachira49@gmail.com>
2025-04-11 11:57:50 +0300

    Update randomwall.js

--- plugins/randomwall.js
+++ plugins/randomwall.js
@@ -17,7 +17,7 @@ cmd({
     const { data } = await axios.get(apiUrl);
     
     if (data.status && data.imgUrl) {
-      const caption = `🌌 *Random Wallpaper: ${query}*\n\n> *© Poᴡᴇʀᴇᴅ Bʏᴇ Sʜᴀʙᴀɴ Mᴅ*`;
+      const caption = `🌌 *Random Wallpaper: ${query}*\n\n> *© Poᴡᴇʀᴇᴅ Bʏ NEXUS-XMD*`;
       await conn.sendMessage(from, { image: { url: data.imgUrl }, caption }, { quoted: m });
     } else {
       reply(`❌ No wallpaper found for *"${query}"*.`);
