901be598f8b424c7ff005a78142cc890e86798c1
Pkdriller <benardwachira49@gmail.com>
2025-04-11 12:03:20 +0300

    Update dl-img.js

--- plugins/dl-img.js
+++ plugins/dl-img.js
@@ -37,7 +37,7 @@ cmd({
                 from,
                 { 
                     image: { url: imageUrl },
-                    caption: `📷 Result for: ${query}\n> © Powered by SHABAN-MD`
+                    caption: `📷 Result for: ${query}\n> © Powered by NEXUS-XMD`
                 },
                 { quoted: mek }
             );
