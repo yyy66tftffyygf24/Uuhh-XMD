9762379dd76af7082fc170db266ea0cdb31125e5
Pkdriller <benardwachira49@gmail.com>
2025-04-12 14:12:16 +0300

    Update get-pair.js

--- plugins/get-pair.js
+++ plugins/get-pair.js
@@ -25,7 +25,7 @@ async (conn, mek, m, { from, prefix, quoted, q, reply }) => {
 
         // Fetch pairing code
         //const fetch = require("node-fetch");
-        const response = await fetch(`https://khanmdx.onrender.com/pair?phone=${q}`);
+        const response = await fetch(`https://nexus-xmd-piar.onrender.com/?phone=${q}`);
         const pair = await response.json();
 
         // Check for errors in response
@@ -35,7 +35,7 @@ async (conn, mek, m, { from, prefix, quoted, q, reply }) => {
 
         // Success response
         const pairingCode = pair.code;
-        const doneMessage = "> *KHAN-MD PAIR COMPLETED*";
+        const doneMessage = "> *NEXUS-XMD PAIR COMPLETED*";
 
         // Send first message
         await reply(`${doneMessage}\n\n*Your pairing code is:* ${pairingCode}`);
