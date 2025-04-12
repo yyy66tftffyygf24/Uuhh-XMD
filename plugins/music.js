0d1a4fe16295cfedcc66808571d8381fea30a954
Pkdriller <benardwachira49@gmail.com>
2025-04-12 03:05:45 +0300

    Update music.js

--- plugins/music.js
+++ plugins/music.js
@@ -17,7 +17,7 @@ cmd({
     return reply("Please provide a title or link (Spotify/YouTube)!");
   }
 
-  reply("> ᴄᴀsᴇʏʀʜᴏᴅᴇs xᴍᴅ🫅ɪs ғᴇᴛᴄʜɪɴɢ ᴀᴜᴅɪᴏ... 🎧");
+  reply("> NEXUS-XMD ɪs ғᴇᴛᴄʜɪɴɢ ᴀᴜᴅɪᴏ... 🎧");
 
   let spotifySent = false;
   let youtubeSent = false;
@@ -43,7 +43,7 @@ cmd({
           contextInfo: {
             externalAdReply: {
               title: spotifyTrack.title,
-              body: "ᴄᴀsᴇʏʀʜᴏᴅᴇs xᴍᴅ🫅 🥰💖: sᴘᴏᴛɪғʏ",
+              body: "NEXUS-XMD🇰🇪: sᴘᴏᴛɪғʏ",
               mediaType: 1,
               sourceUrl: spotifyTrack.url,
               renderLargerThumbnail: true
@@ -76,7 +76,7 @@ cmd({
           contextInfo: {
             externalAdReply: {
               title: youtubeVideo.title,
-              body: "ᴄᴀsᴇʏʀʜᴏᴅᴇs xᴍᴅ🫅🥰: ʏᴏᴜᴛᴜʙᴇ",
+              body: "NEXUS-XMD🇰🇪: ʏᴏᴜᴛᴜʙᴇ",
               mediaType: 1,
               sourceUrl: youtubeVideo.url,
               renderLargerThumbnail: true
@@ -99,8 +99,8 @@ cmd({
   } else if (spotifySent && youtubeSent) {
     reply("Both Spotify and YouTube audio sent successfully.");
   } else if (spotifySent) {
-    reply("ᴄᴀsᴇʏʀʜᴏᴅᴇs xᴍᴅ🫅: sᴘᴏᴛɪғʏ ᴀᴜᴅɪᴏ sᴇɴᴛ sᴜᴄᴄᴇsғᴜʟʟʏ.");
+    reply("NEXUS-XMD🇰🇪: sᴘᴏᴛɪғʏ ᴀᴜᴅɪᴏ sᴇɴᴛ sᴜᴄᴄᴇsғᴜʟʟʏ.");
   } else if (youtubeSent) {
-    reply("ᴄᴀsᴇʏʀʜᴏᴅᴇs ᴄᴍᴅ: ʏᴏᴜᴛᴜʙᴇ ᴀᴜᴅɪᴏ sᴇɴᴛ sᴜᴄᴄᴇsғᴜʟʟʏ.");
+    reply("NEXUS-XMD: ʏᴏᴜᴛᴜʙᴇ ᴀᴜᴅɪᴏ sᴇɴᴛ sᴜᴄᴄᴇsғᴜʟʟʏ.");
   }
 });
