82702eaceaef87eff7b92a7d39254de26afef5ee
Pkdriller <benardwachira49@gmail.com>
2025-04-12 10:59:04 +0300

    Update main.js

--- plugins/main.js
+++ plugins/main.js
@@ -253,7 +253,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 
         // Send audio
         await conn.sendMessage(from, {
-            audio: { url: 'https://github.com/devhanstz/VORTEX-XMD-DATA/raw/refs/heads/main/KingHans/Menu.mp3' },
+            audio: { url: 'https://files.catbox.moe/lpggim.mp3' },
             mimetype: 'audio/mp4',
             ptt: true
         }, { quoted: mek });
