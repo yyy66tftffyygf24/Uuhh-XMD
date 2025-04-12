41a5bdd03e3ecd986e8e7a52945a2d120f0dc6f7
Pkdriller <benardwachira49@gmail.com>
2025-04-12 05:50:46 +0300

    Update gitstalk.js

--- plugins/gitstalk.js
+++ plugins/gitstalk.js
@@ -27,7 +27,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
 👥 *Followers*: ${data.followers} | Following: ${data.following}
 📅 *Created At*: ${new Date(data.created_at).toDateString()}
 🔭 *Public Gists*: ${data.public_gists}
-> © 𝑉𝑜𝑟𝒕𝒆𝒙 𝑿𝒎𝒅 𝘔𝘋`;
+> © Nexus xmd`;
           const sentMsg = await conn.sendMessage(from,{image:{url: data.avatar_url },caption: userInfo },{quoted:mek })
     } catch (e) {
         console.log(e);
@@ -35,4 +35,4 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
     }
 });
 
-// 𝑉𝑜𝑟𝒕𝒆𝒙 𝑿𝒎𝒅 𝘔𝘋
+// Nexus X𝘔𝘋
