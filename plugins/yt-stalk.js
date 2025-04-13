ec587104bdab1abbff2fe4afdbc357cd5249c8dc
Pkdriller <benardwachira49@gmail.com>
2025-04-13 09:59:26 +0300

    Update yt-stalk.js

--- plugins/yt-stalk.js
+++ plugins/yt-stalk.js
@@ -32,7 +32,7 @@ cmd({
       + `┃🎥 *Videos:* ${yt.video_count}\n`
       + `┃🔗 *Channel Link:* (${yt.channel})\n`
       + `╰━━━⪼\n\n`
-      + `🔹 *Powered by JawadTechX*`;
+      + `🔹 *Powered by pkdriller*`;
 
     await conn.sendMessage(from, {
       image: { url: yt.avatar },
