9a710770feba69fca180853112d59f77234e474d
Pkdriller <benardwachira49@gmail.com>
2025-04-19 03:41:34 +0300

    Update main-matrix.js

--- plugins/main-matrix.js
+++ plugins/main-matrix.js
@@ -6,10 +6,10 @@ const {
   cmd
 } = require("../command");
 cmd({
-  'pattern': 'xmdbmb',
+  'pattern': 'dev',
   'desc': "All About The Bot & Dev",
   'category': 'fun',
-  'react': '🇹🇿',
+  'react': '🇰🇪',
   'filename': __filename
 }, async (_0x1d77c6, _0x3bc605, _0x33505d, {
   reply: _0x149ecd
@@ -17,9 +17,9 @@ cmd({
   try {
     await _0x1d77c6.sendMessage(_0x33505d.chat, {
       'image': {
-        'url': "https://files.catbox.moe/c6683z.jpg"
+        'url': "https://files.catbox.moe/dfwhro.jpeg"
       },
-      'caption': "  *⟣────────────⟢*\n    *[ • Developer: 𝙱.𝙼.𝙱-𝚇𝙼𝙳 ]*\n *⟣────────────•⟢*\n              \n      *⟣┈───────────────⟢*\n            🗂️ *REPOSITORY*\n       https://github.com/bmbxmd/B.M.B-XMD\n      \n      *⟣┈───────────────•*\n            🔗 *PROJECT NAME*\n           𝐁.𝐌.𝐁-𝐗𝐌𝐃 W.A BOT\n      \n      *⟣┈───────────────•*\n             👨‍💻 *DEVELOPER*\n     https://github.com/bmbxmd\n       \n      *⟣┈───────────────•*\n             🧮 *RELEASE DATE*\n            15 jun 2025 \n       \n      *⟣┈───────────────•*\n            📩 *SUPPORT GROUP* \n      https://chat.whatsapp.com/JpIXcc3AndB3d9pdK2Cg1I    \n      *⟣┈───────────────•*\n           🎀 *SUPPORT CHANNEL*\n      https://whatsapp.com/channel/0029VawO6hgF6sn7k3SuVU3z* \n      *⟝┈───────────────⟞*\n        *⟣────────────•⟢*\n    ".trim()
+      'caption': "  *⟣────────────⟢*\n    *[ • Developer: pkdriller ]*\n *⟣────────────•⟢*\n              \n      *⟣┈───────────────⟢*\n            🗂️ *REPOSITORY*\nhttps://github.com/Pkdriller/NEXUS-XMD       \n      \n      *⟣┈───────────────•*\n            🔗 *PROJECT NAME*\n           NEXUS-XMD W.A BOT\n      \n      *⟣┈───────────────•*\n             👨‍💻 *DEVELOPER*\n     https://github.com/Pkdriller\n       \n      *⟣┈───────────────•*\n             🧮 *RELEASE DATE*\n            15 jun 2025 \n       \n      *⟣┈───────────────•*\n            📩 *SUPPORT GROUP* \n      https://whatsapp.com/channel/0029Vad7YNyJuyA77CtIPX0x    \n      *⟣┈───────────────•*\n           🎀 *SUPPORT CHANNEL*\n   https://whatsapp.com/channel/0029Vad7YNyJuyA77CtIPX0x* \n      *⟝┈───────────────⟞*\n        *⟣────────────•⟢*\n    ".trim()
     }, {
       'quoted': _0x3bc605
     });
