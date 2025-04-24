6010c3b5113c4d58a0729b7fd27ac43e4b67ddca
🐐 Mr pkdriller 🐐 <benardwachira49@gmail.com>
2025-04-24 23:33:16 +0300

    Update doba na radama.js

--- plugins/doba na radama.js
+++ plugins/doba na radama.js
@@ -3,7 +3,7 @@ const yts = require('yt-search')
 const axios = require("axios");
 
 cmd({
-    pattern: "play1",
+    pattern: "play",
     desc: "To download songs.",
     react: "🎵",
     category: "download",
