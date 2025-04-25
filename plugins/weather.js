cbf0b6b79ec70b8ca706f8f7b8bcc37d0692c289
🐐 Mr pkdriller 🐐 <benardwachira49@gmail.com>
2025-04-25 21:45:48 +0300

    Update weather.js

--- plugins/weather.js
+++ plugins/weather.js
@@ -28,7 +28,7 @@ async (conn, mek, m, { from, q, reply }) => {
 > 💨 *Wind Speed*: ${data.wind.speed} m/s
 > 🔽 *Pressure*: ${data.main.pressure} hPa
 
-> *© 𝐁.𝐌.𝐁-𝐗𝐌𝐃✅*
+> *© NEXUS-XMD ✅*
 `;
         return reply(weather);
     } catch (e) {
