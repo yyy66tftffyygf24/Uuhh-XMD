40aa1c6953788c48c6d7337643268681a8faf54c
🐐 Mr pkdriller 🐐 <benardwachira49@gmail.com>
2025-04-25 13:18:38 +0300

    Add files via upload

--- plugins/owner.js
+++ plugins/owner.js
@@ -3,7 +3,7 @@ const config = require('../config');
 
 cmd({
     pattern: "owner",
-    react: "🧶", 
+    react: "🚀", 
     desc: "Get owner number",
     category: "main",
     filename: __filename
@@ -29,7 +29,7 @@ async (conn, mek, m, { from }) => {
 
         // Send the owner contact message with image and audio
         await conn.sendMessage(from, {
-            image: { url: 'https://i.ibb.co/NnmFGrq3/nexus-xmd.jpg' }, // Image URL from your request
+            image: { url: 'https://i.ibb.co/j9ShBQP5/nexus-xmd.jpg' }, // Image URL from your request
             caption: `
 ╭━〔 *NEXUS-𝐗𝐌𝐃* 〕━┈⊷
 ┃◈╭─────────────·๏
@@ -54,7 +54,7 @@ async (conn, mek, m, { from }) => {
 
         // Send audio as per your request
         await conn.sendMessage(from, {
-            audio: { url: 'https://files.catbox.moe/ersq63.mp3' }, // Audio URL
+            audio: { url: '' }, // Audio URL
             mimetype: 'audio/mp4',
             ptt: true
         }, { quoted: mek });
