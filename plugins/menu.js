3f211e6529105a5aef2d16b055622cbeea73c63a
🐐 Mr pkdriller 🐐 <benardwachira49@gmail.com>
2025-04-25 12:29:51 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -5,7 +5,7 @@ const { runtime } = require('../lib/functions');
 const axios = require('axios');
 
 cmd({
-    pattern: "menu1",
+    pattern: "menu",
     desc: "menu the bot",
     category: "menu1",
     react: "⚡",
