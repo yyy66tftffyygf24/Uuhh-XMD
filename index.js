5ca343ed1aea7bddef68b6fa9fe5509008c2042b
🐐 Mr pkdriller 🐐 <benardwachira49@gmail.com>
2025-05-17 01:21:34 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ const {
   //===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/sessions/creds.json')) {
 if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
-const sessdata = config.SESSION_ID.replace("POPKID~", '');
+const sessdata = config.SESSION_ID.replace("nexus~", '');
 const filer = File.fromURL(${sessdata}`)
 filer.download((err, data) => {
 if(err) throw err
